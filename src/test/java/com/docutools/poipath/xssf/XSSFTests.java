06dda940d917d75f7be00b5dbb7a9568f7113a19
Alexander Partsch <a.partsch@aeiou-software.at>
2021-03-23 12:34:46 +0100

    Fixes #6 Warnings in Test Cases

--- src/test/java/com/docutools/poipath/xssf/XSSFTests.java
+++ src/test/java/com/docutools/poipath/xssf/XSSFTests.java
@@ -1,43 +1,40 @@
 package com.docutools.poipath.xssf;
 
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.Objects;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.closeTo;
 import static org.hamcrest.Matchers.equalTo;
 
-import java.io.IOException;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
 @DisplayName("Excel Tests")
 class XSSFTests {
-  XSSFWorkbook workbook;
 
-  @AfterEach
-  void cleanup() throws IOException {
-    if (workbook != null) {
-      workbook.close();
+  @Test
+  void simpleNavigationTest() throws IOException {
+    try (var stream = Objects.requireNonNull(XSSFTests.class.getResourceAsStream("/XSSF/simpleTest.xlsx"))) {
+      var workbook = new XSSFWorkbook(stream);
+      var text = new XSSFWorkbookWrapper(workbook).sheet("OG").row(0).cell(0).content();
+
+      assertThat(text, equalTo("No Peace"));
     }
   }
 
   @Test
-  void simpleNavigationTest() throws IOException {
-    workbook = new XSSFWorkbook(XSSFTests.class.getResourceAsStream("/XSSF/simpleTest.xlsx"));
-    var text = new XSSFWorkbookWrapper(workbook).sheet("OG").row(0).cell(0).content();
-
-    assertThat(text, equalTo("No Peace"));
-  }
-
-  @Test
   void doubleTest() throws IOException {
-    workbook = new XSSFWorkbook(XSSFTests.class.getResourceAsStream("/XSSF/numberTest.xlsx"));
-    var cell = new XSSFWorkbookWrapper(workbook).sheet("OG").row(0).cell(0);
-    var stringContent = cell.content();
-    var doubleContent = cell.doubleValue();
+    try (var stream = Objects.requireNonNull(XSSFTests.class.getResourceAsStream("/XSSF/numberTest.xlsx"))) {
+      var workbook = new XSSFWorkbook(stream);
+      var cell = new XSSFWorkbookWrapper(workbook).sheet("OG").row(0).cell(0);
+      var stringContent = cell.content();
+      var doubleContent = cell.doubleValue();
 
-    assertThat(stringContent, equalTo("1312.0"));
-    assertThat(doubleContent, closeTo(1312.0, 0.1));
+      assertThat(stringContent, equalTo("1312.0"));
+      assertThat(doubleContent, closeTo(1312.0, 0.1));
+    }
   }
 
 }
--- src/test/java/com/docutools/poipath/xwpf/XWPFTests.java
+++ src/test/java/com/docutools/poipath/xwpf/XWPFTests.java
@@ -1,39 +1,36 @@
 package com.docutools.poipath.xwpf;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-
-import java.io.IOException;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.util.Objects;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
 @DisplayName("Word Tests")
 class XWPFTests {
-  XWPFDocument document;
 
-  @AfterEach
-  void cleanup() throws IOException {
-    if (document != null) {
-      document.close();
+  @Test
+  void simpleNavigationTest() throws IOException {
+    try (var stream = Objects.requireNonNull(XWPFTests.class.getResourceAsStream("/XWPF/simpleTest.docx"))) {
+      var document = new XWPFDocument(stream);
+      var text = new XWPFDocumentWrapper(document).paragraph(0).text();
+
+      assertThat(text, equalTo("No Justice"));
     }
   }
 
   @Test
-  void simpleNavigationTest() throws IOException {
-    document = new XWPFDocument(XWPFTests.class.getResourceAsStream("/XWPF/simpleTest.docx"));
-    var text = new XWPFDocumentWrapper(document).paragraph(0).text();
-
-    assertThat(text, equalTo("No Justice"));
-  }
-
-  @Test
   void getPictureTest() throws IOException {
-    document = new XWPFDocument(XWPFTests.class.getResourceAsStream("/XWPF/pictureTest.docx"));
-    var picture = new XWPFDocumentWrapper(document).paragraph(0).run(0).pictures();
+    try (var stream = Objects.requireNonNull(XWPFTests.class.getResourceAsStream("/XWPF/pictureTest.docx"))) {
+      var document = new XWPFDocument(stream);
+      var picture = new XWPFDocumentWrapper(document).paragraph(0).run(0).pictures();
 
-    assertThat(picture.size(), equalTo(1));
+      assertThat(picture.size(), equalTo(1));
+    }
   }
 
 }
